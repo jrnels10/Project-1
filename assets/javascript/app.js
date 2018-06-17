25133dca241f69226ecda03037f354851aa16cd6
Jacob <jrnels10@gmail.com>
2018-06-16 20:43:32 -0700

    locater onclick works. returns full address though

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 require([
+    "esri/tasks/Locator",
     // loads code specific to creating a map
     "esri/Map",
     // loads code that allows for viewing the map in 2D(Switch MapView to SceneView to turn map 3D)
@@ -7,9 +8,14 @@ require([
     // ensures the DOM is available before executing code.
     "dojo/domReady!"
 ], function (
+    Locator,
     Map,
     SceneView,
     Search) {
+
+        var locatorTask = new Locator({
+            url: "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer"
+        })
 
         var map = new Map({
             basemap: "streets",
@@ -17,7 +23,7 @@ require([
         });
 
         var view = new SceneView({
-            scale: 123456789,
+            scale: 24000,
             container: "viewDiv",
             map: map
         });
@@ -30,5 +36,31 @@ require([
         view.ui.add(searchWidget, {
             position: "top-right"
         });
+
+
+        view.on("click", function (event) {
+            event.stopPropagation();
+
+            // Get the coordinates of the click on the view
+            // around the decimals to 3 decimals
+            var lat = Math.round(event.mapPoint.latitude * 1000) / 1000;
+            var lon = Math.round(event.mapPoint.longitude * 1000) / 1000;
+
+            view.popup.open({
+                // Set the popup's title to the coordinates of the clicked location
+                title: "Reverse geocode: [" + lon + ", " + lat + "]",
+                location: event.mapPoint // Set the location of the popup to the clicked location
+            });
+            // Execute a reverse geocode using the clicked location
+            locatorTask.locationToAddress(event.mapPoint).then(function (response) {
+                console.log(response.FindAsync)
+                // If an address is successfully found, show it in the popup's content
+                view.popup.content = response.address;
+                console.log(view.popup.content)
+            }).catch(function (err) {
+                // If the promise fails and no result is found, show a generic message
+                view.popup.content = "No address was found for this location";
+            });
+        });
     });
-    console.log('hello')
+console.log('hello')
