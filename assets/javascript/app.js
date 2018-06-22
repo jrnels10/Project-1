87ac048f7c17f8d60afeeb26344a080fa438c18f
Xeroneon <andrewvasquez0113@gmail.com>
2018-06-21 19:45:39 -0700

    messing with map popup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -47,12 +47,12 @@ require([
             position: "top-right"
         });
         searchWidget.on("search-complete", function (event) {
-	    //Center map to closest location
+            //Center map to closest location
             var lat = event.results[0].results[0].extent.center.latitude; //get lat from 1st address result
-	    var long = event.results[0].results[0].extent.center.longitude; //get long from 1st address result
+            var long = event.results[0].results[0].extent.center.longitude; //get long from 1st address result
 
-	    centerMap(view, Point, lat, long); //center map
-		
+            centerMap(view, Point, lat, long); //center map
+
             console.log("Search started.");
             console.log("results", event)
             console.log("result", event.target.searchTerm)
@@ -79,13 +79,25 @@ require([
                 lon: lon
             })
 
+            
             meetupAPI();
+            setTimeout(function() {
+                database.ref().once("value").then(function (snap) {
+                    var eventName;
+                    eventName = snap.val().eventName
+                    view.popup.open({
+                        // Set the popup's title to the coordinates of the clicked location
+                        title: eventName,
+                        content: lat + " " + lon,
+                        location: {latitude: snap.val().eventLat, longitude: snap.val().eventLon} // Set the location of the popup to the clicked location
+                    });
+                })
+            }, 1000)
 
-            view.popup.open({
-                // Set the popup's title to the coordinates of the clicked location
-                title: "Reverse geocode: [" + lon + ", " + lat + "]",
-                location: event.mapPoint // Set the location of the popup to the clicked location
-            });
+            console.log("map", event.mapPoint);
+            
+            
+
             // Execute a reverse geocode using the clicked location
             locatorTask.locationToAddress(event.mapPoint).then(function (response) {
                 console.log("response", response)
@@ -102,20 +114,20 @@ require([
                 // If the promise fails and no result is found, show a generic message
                 view.popup.content = "No address was found for this location";
             });
-            
+
             centerMap(view, Point, lat, lon);
         });
     });
 
-function centerMap(view, Point, lat, lon)
-{
-	var pt = new Point({
-  		latitude: lat,
-  		longitude: lon
-	});
-    
-	// go to the given point
-	view.goTo(pt);
+function centerMap(view, Point, lat, lon) {
+
+    var pt = new Point({
+        latitude: lat,
+        longitude: lon
+    });
+
+    // go to the given point
+    view.goTo(pt);
 }
 
 console.log('hello')
@@ -160,7 +172,7 @@ database.ref().update({
 //             method: 'GET'
 
 //         }).then(function (response) {
-        
+
 //             for (i = 0; i < 10; i++) {
 
 //                 var results = response.data;
@@ -219,29 +231,28 @@ database.ref().update({
 // ==================================================================================================
 
 
-var meetupAPI = function() {
+var meetupAPI = function () {
 
-    database.ref().once("value").then(function(snap) {
+    database.ref().once("value").then(function (snap) {
         var url = "https://api.meetup.com/find/upcoming_events?&key=413e32034783f3038f567864804610&lat=" + snap.val().lat + "&lon=" + snap.val().lon + "&sign=true&photo-host=public&page=20";
         $.ajax({
-                
-            dataType:'jsonp',
-            method:'get',
-            url:url,
-            success:function(result) {
+
+            dataType: 'jsonp',
+            method: 'get',
+            url: url,
+            success: function (result) {
                 // console.log('back with ' + result.data.length +' results');
                 console.log(result);
-                var signedURL = result.meta.signed_url;
-                for (var i = 0; i < signedURL.length; i++) {
-                var meetupDetails =[];
-                var grouplabel = signedURL[i].group.name;
-                var groupLat = signedURL[i].group.lat;
-                var groupLon = signedURL[i].group.lon;
-                console.log('group: ' + grouplabel + ', lat: ' + groupLat + ', lon: ' + groupLon);
+
+                database.ref().update({
+                    eventName: result.data.events["6"].name,
+                    eventLat: result.data.events["6"].venue.lat ,
+                    eventLon: result.data.events["6"].venue.lon
+                })
+
             }
-        });	
+        });
 
     })
 
 }
-
