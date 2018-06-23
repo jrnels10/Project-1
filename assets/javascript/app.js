533af7162e262c0f2d8ddfb9165b3a7e6fbdeaf4
Xeroneon <andrewvasquez0113@gmail.com>
2018-06-23 12:42:28 -0700

    added a function to parse military time into regular time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -135,7 +135,7 @@ require([
                 symbol: markerSymbol,
                 popupTemplate: { // autocasts as new PopupTemplate()
                     title: "<a target='_blank' href='" + snap.val().eventLink + "'>" + snap.val().eventName + "</a>",
-                    content: "<p>Group: " + snap.val().eventGroupName + "</p><p>Time: " + snap.val().eventTime + " Date: " + snap.val().eventDate + "</p>"
+                    content: "<p>Group: " + snap.val().eventGroupName + "</p><p>Time: " + parseTime(snap.val().eventTime) + " Date: " + snap.val().eventDate + "</p>"
                     + "<p> RSVP Count: " + snap.val().eventRsvpCount + "  Waitlist: " + snap.val().eventWaitlist + "</p>"
                   }
             });
@@ -237,4 +237,32 @@ var meetupAPI = function () {
 
     })
 
+}
+
+var parseTime = function(timeInput) {
+    var time = timeInput; // your input
+    
+    time = time.split(':'); // convert to array
+    
+    // fetch
+    var hours = Number(time[0]);
+    var minutes = Number(time[1]);
+    // var seconds = Number(time[2]);
+    
+    // calculate
+    var timeValue;
+    
+    if (hours > 0 && hours <= 12) {
+      timeValue= "" + hours;
+    } else if (hours > 12) {
+      timeValue= "" + (hours - 12);
+    } else if (hours == 0) {
+      timeValue= "12";
+    }
+     
+    timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+    // timeValue += (seconds < 10) ? ":0" + seconds : ":" + seconds;  // get seconds
+    timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+
+    return timeValue;
 }
