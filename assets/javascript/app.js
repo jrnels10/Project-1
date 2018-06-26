9b7bc7e8f193657534d593b6947b498e26e6d990
Jacob <jrnels10@gmail.com>
2018-06-25 19:56:27 -0700

    popup colors have changed

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -135,10 +135,10 @@ require([
             // Create a symbol for drawing the point
             var markerSymbol = {
                 type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
-                color: [170, 66, 244],
+                color: [176,0,178],
                 outline: { // autocasts as new SimpleLineSymbol()
-                    color: [255, 255, 255],
-                    width: 2
+                    color: [56,3,86],
+                    width: 1
                 }
             };
             // markerSymbol.addClass('test');
@@ -149,7 +149,7 @@ require([
                 symbol: markerSymbol,
                 popupTemplate: { // autocasts as new PopupTemplate()
 
-                    title: "<a target='_blank' href='" + snap.val().eventLink + "'>" + snap.val().eventName + "</a>",
+                    title: "<a class='pop-up-title' target='_blank' href='" + snap.val().eventLink + "'>" + snap.val().eventName + "</a>",
                     content: "<p>Group: " + snap.val().eventGroupName + "</p><p>Time: " + snap.val().eventTime + "</p><p> Date: " + snap.val().eventDate + "</p>"
                     + rsvpTag
                   }
