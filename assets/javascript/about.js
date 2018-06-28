bb82783b9f18db9bd58ceeb6dd3bce0496f3c7f3
stevenpassey <38443098+stevenpassey@users.noreply.github.com>
2018-06-28 14:45:26 -0700

    Javascript code for mobile responsive about page

--- assets/javascript/about.js
+++ assets/javascript/about.js
@@ -5,9 +5,6 @@ $('.carousel.carousel-slider').carousel({
 
   window.onload = function () { 
 
-    
-  
-  
     //check size for menu
     if(window.innerWidth <= 992)
     {
@@ -22,6 +19,9 @@ $('.carousel.carousel-slider').carousel({
   
       $("body").append("<div id='hamburger' " + styleHeight + " onClick='menuTime()'>&#9776;</div>");
     }
+
+	sizeCarouselItems("load");
+
   };
 
   var menuOnTheGrill = false;
@@ -71,4 +71,92 @@ window.onresize = function () {
 			$("#hamburger").remove();
 		}
 	}
+
+	sizeCarouselItems("size");
 };
+
+var imageRatioArray = [];
+
+function sizeCarouselItems(caller)
+{
+	////////////////////Size Carousel Items/////////////////////////////////
+	if(window.innerWidth < 900)
+	{
+		var extraHeight = 195;
+	}
+	else
+	{
+		var extraHeight = 260;
+	}
+
+	var textHeightArray = [];
+	var carouselHeight = $(".carousel")[0].offsetHeight - extraHeight;
+	var imageHeightArray = [];
+
+	var $carouselTextItem = [$("#item-text-jacob")[0],$("#item-text-andrew")[0],$("#item-text-steven")[0],$("#item-text-paulo")[0]];
+
+	[0,1,2,3].forEach(function(i) {
+	 	$carouselTextItem[i].style.height = "auto";
+	});
+
+	for(var carouselTextNumber in $carouselTextItem)
+	{
+		textHeightArray.push($carouselTextItem[carouselTextNumber].offsetHeight);
+	}
+
+	var $carouselImageItem = [$(".bio-pic")[0],$(".bio-pic")[1],$(".bio-pic")[2],$(".bio-pic")[3]];
+
+
+	for(var carouselImageNumber in $carouselImageItem)
+	{
+		imageHeightArray.push($carouselImageItem[carouselImageNumber].offsetHeight);
+	}
+
+	imageHeightArray.map(function(height) {
+
+		var imageIndex = imageHeightArray.indexOf(height);
+  		imageHeightArray[imageIndex] = carouselHeight - textHeightArray[imageIndex];
+
+		if(imageHeightArray[imageIndex] < 1)
+		{
+			imageHeightArray[imageIndex] = 1;
+		}
+	});
+
+	textHeightArray.map(function(height) {
+
+		var textIndex = textHeightArray.indexOf(height);
+  		textHeightArray[textIndex] = carouselHeight - imageHeightArray[textIndex];
+
+		if(textHeightArray[textIndex] + $carouselImageItem[textIndex].offsetTop > window.innerHeight)
+		{
+			$("body").css("overflow", "scroll");
+		}
+		else
+		{
+			$("body").css("overflow", "hidden");
+		}
+
+	});
+
+	[0,1,2,3].forEach(function(i) {
+ 		$carouselTextItem[i].style.height = textHeightArray[i] + "px";
+ 		$carouselImageItem[i].style.height = imageHeightArray[i] + "px";
+
+	});
+
+
+	if(window.innerHeight < 644)
+	{
+		var currentLocation = $(".indicators")[0].offsetTop;
+		var neededLocation = -5;
+		var delta = currentLocation - neededLocation;
+
+		$(".indicators").css("transform", "translateY(-" + delta + "px)");
+	}
+	else
+	{
+		$(".indicators").css("transform", "translateY(0px)");
+	}
+
+}
